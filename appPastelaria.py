c66530fc87599d09b681a3572520abdeaea3bcc7
tininhaasa <tininhakook@gmail.com>
2022-09-28 20:43:10 -0300

    criando o basico do front

---
+++ appPastelaria.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+from mod_funcionarios.funcionario import bp_funcionario
+from mod_clientes.clientes import bp_cliente
+from mod_produtos.produtos import bp_produto
+from mod_index.index import bp_index
+
+app = Flask(__name__)
+
+''' chamadas dos formulários '''
+
+# registro das rotas do blueprint
+app.register_blueprint(bp_funcionario)
+app.register_blueprint(bp_cliente)
+app.register_blueprint(bp_produto)
+app.register_blueprint(bp_index)
+
+if __name__ == "__main__":
+    """ Inicia o aplicativo WEB Flask """
+    app.run(host='0.0.0.0', port=5000, debug=True)
